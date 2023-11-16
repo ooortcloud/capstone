5f8e1f47ad480bbad954c0faa9f9d8a487986552
ooortcloud <110080440+ooortcloud@users.noreply.github.com>
2023-11-16 20:40:00 +0900

    init

---
+++ capstone/src/main/java/dev/capstone/service/FacadeService.java
@@ -0,0 +1,12 @@
+package dev.capstone.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FacadeService {
+
+
+
+}
